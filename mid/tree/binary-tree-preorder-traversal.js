ff94a5bbec8cbe4cdb05bcf52ee68c94d0f98dfc
ReisenD <279303730@qq.com>
2020-10-27 11:23:49 +0800

    二叉树前序遍历-迭代

--- mid/tree/binary-tree-preorder-traversal.js
+++ mid/tree/binary-tree-preorder-traversal.js
@@ -38,3 +38,30 @@ var preorderTraversal = function(root) {
 };
 
 console.log(preorderTraversal(root))
+
+// 解法：迭代
+// 1、创建一个栈
+// 2、到达一个节点时，记录值，并将其压入栈内，并查看其左节点
+// 3、当节点不存在，但栈不为空时，将最后一个元素出栈，并开始查看其右节点，若右节点存在，执行 (2)
+// 4、当左右节点皆空，继续出栈执行 (3)，直至栈与节点都没有了，完成遍历
+
+var preorderTraversal2 = function(root) {
+    let ans = []
+    if(root == null) {
+        return ans
+    }
+
+    let stack = [];
+    let node = root
+    while (stack.length || node) {
+        while (node) {
+            ans.push(node.val)
+            stack.push(node)
+            node = node.left
+        }
+        node = stack.pop()
+        node = node.right
+    }
+    return ans
+};
+
