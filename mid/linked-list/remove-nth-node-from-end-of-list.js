54049dce277790fa9d59d654e6a2330f8f75f45e
reisenDH <279303730@qq.com>
2021-01-17 23:03:10 +0800

    删除链表倒数第n节点

--- mid/linked-list/remove-nth-node-from-end-of-list.js
+++ mid/linked-list/remove-nth-node-from-end-of-list.js
@@ -1,14 +1,12 @@
+// https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
 // 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
 // 给定一个链表: 1->2->3->4->5, 和 n = 2.
 // 当删除了倒数第二个节点后，链表变为 1->2->3->5.
 
-**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
 /**
  * @param {ListNode} head
  * @param {number} n
@@ -44,3 +42,37 @@ var removeNthFromEnd = function(head, n) {
     head = head.next
     return head
 };
+
+// 思路：
+// 1. 使用虚拟头部，从0开始，原函数位置即为1
+// 2. 双指针，一快一慢，两个指针间隔 n+1
+// 3. 当快指针到达最后，慢指针 slow 的位置就是目标删除位置的前一位
+// 4. 执行 next = next.next
+var removeNthFromEnd = function(head, n) {
+    let Dhead = new ListNode(0)
+    Dhead.next = head
+    let fast = Dhead;
+    let slow = Dhead;
+    let fastStep = 0;
+    while(fast.next !== null) {
+        fast = fast.next;
+        fastStep += 1
+        if (fastStep >= n+1) {
+            slow = slow.next
+        }
+    }
+    slow.next = slow.next.next
+    return Dhead.next
+}
+
+let head = new ListNode(1, 
+    // new ListNode(2, 
+    //     new ListNode(3, 
+    //         new ListNode(4,
+    //             new ListNode(5)
+    //         ) 
+    //     )
+    // )
+)
+
+removeNthFromEnd(head, 1)
